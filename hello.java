7f74b48f090762b4d189b43b57787ec9f1216b6f
Andressa <90698965+andressarag@users.noreply.github.com>
2025-08-14 20:48:16 -0300

    Atualizar o hello.java

--- hello.java
+++ hello.java
@@ -1,3 +1,4 @@
+package edu.fatec
 Public class HelloWord{
   public static void main(string []args){
    System.out.print("Hello world");
@@ -6,14 +7,19 @@ Public class HelloWord{
 
 usar prompt do Pc para abrir arquivo: 
 notepad HelloWord.java
-Java c para compilar 
-Java nome do arquivo p rodar 
+javac .edu/Fatec/*.Java
+java edu.fatec.helloword
 
-para as aulas vamos usar pastas 
+
+para as aulas vamos usar pastas criar pelo prompt 
 c:\POO (dentro da pasta medir edu, entrar cd edu)
-  edu
-   Fatec
-    helloWord.java (notepad HelloWord.java)
+  alunos 
+   edu
+    fatec
+     Alunos.java(classe)
+     TesteAluno.java(classe)
+    
+
 
 
 
