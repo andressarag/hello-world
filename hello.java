cb665b33bebdf892e58160b3fc6b1d67e5109fba
Andressa <90698965+andressarag@users.noreply.github.com>
2025-08-14 20:35:08 -0300

    Criar hello.java

---
+++ hello.java
@@ -0,0 +1,21 @@
+Public class HelloWord{
+  public static void main(string []args){
+   System.out.print("Hello world");
+  }
+}
+
+usar prompt do Pc para abrir arquivo: 
+notepad HelloWord.java
+Java c para compilar 
+Java nome do arquivo p rodar 
+
+para as aulas vamos usar pastas 
+c:\POO (dentro da pasta medir edu, entrar cd edu)
+  edu
+   Fatec
+    helloWord.java (notepad HelloWord.java)
+
+
+
+
+
